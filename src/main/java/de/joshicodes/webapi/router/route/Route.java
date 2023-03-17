64d227568a3d007f7224c95df170377405812e1c
Joshua <joshua@joshi.codes>
2023-03-17 17:15:20 +0100

    changed Route from abstract class to interface

--- src/main/java/de/joshicodes/webapi/router/route/Route.java
+++ src/main/java/de/joshicodes/webapi/router/route/Route.java
@@ -3,10 +3,8 @@ package de.joshicodes.webapi.router.route;
 import de.joshicodes.webapi.request.RequestData;
 import de.joshicodes.webapi.request.ResponseData;
 
-public abstract class Route {
+public interface Route {
 
-    public Route() {}
-
-    abstract public ResponseData handle(RequestData request);
+   ResponseData handle(RequestData request);
 
 }
